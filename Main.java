231c13d1abc82bc7270710277bb7b4faa07dd3e9
DariaSeliverstova <dashulia2004@yandex.ru>
2021-08-13 13:15:23 -0400

    added fils

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+            int bilet;
+                    bilet = 2_000;
+            int rubli;
+                    rubli = 20;
+            int mili;
+                    mili = bilet / rubli;
+            System.out.println(mili);
+}
+}
+
+
